30b0085197cec94a0c68b31b525f17d629eebcb0
MobileJC <jackiechan31590@gmail.com>
2024-10-11 12:17:46 +1000

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,8 +17,8 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    # public $baseURL = 'localhost:8080/';
-    public $baseURL = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+    public $baseURL = 'http://localhost:8080/';
+    # public $baseURL = 'http://' . $_SERVER['HTTP_HOST'] . '/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
